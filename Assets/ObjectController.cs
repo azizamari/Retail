1774529e2d95b758c95ebf1d209c1c3dfa8c7c25
azizamari <azizamari2003@gmail.com>
2020-06-29 20:50:48 +0100

    retail

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,31 @@
+﻿using GoogleARCore.Examples.ObjectManipulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public GameObject[] objects;
+    public PawnManipulator manipulator;
+
+    public void SetTo1()
+    {
+        manipulator.PawnPrefab = objects[0];
+    }
+    public void SetTo2()
+    {
+        manipulator.PawnPrefab = objects[1];
+    }
+    public void SetTo3()
+    {
+        manipulator.PawnPrefab = objects[2];
+    }
+    public void SetTo4()
+    {
+        manipulator.PawnPrefab = objects[3];
+    }
+    public void SetTo5()
+    {
+        manipulator.PawnPrefab = objects[4];
+    }
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
